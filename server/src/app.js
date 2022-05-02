71f894b2630480f90846faede56aeccec0580bf6
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-05-02 11:38:41 +0530

    updated the launch api

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,24 @@
-const express=require('express');
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const cors=require('cors')
+const api = require('./routes/api');
 
-const planetsRouter=require('./routes/planets/planets.router')
+const app = express();
 
-const app=express();
-
-// app.use(cors({
-//     origin:'http://localhost:3000',
-// }));
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(planetsRouter)
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-module.exports= app;
+app.use('/v1', api);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,24 @@
-const http=require('http')
-const express=require('express')
+const http = require('http');
 
-const app=express();
-app.listen()
+require('dotenv').config();
 
-const PORT=process.env.PORT ||8000;
+const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
-const server=http.createServer(app)
+const PORT = process.env.PORT || 8000;
 
-server.listen(PORT, ()=>{
-    console.log(`listening on port ${PORT}...`)
-})
+const server = http.createServer(app);
 
+async function startServer() {
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchData();
+  
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
